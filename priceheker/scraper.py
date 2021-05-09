faa63845a420e78ebe3cd65328824a5305bc2bba
Alex Aman <60818228+Alexamannn@users.noreply.github.com>
2021-05-09 17:18:06 +0200

    Update scraper.py

--- priceheker/scraper.py
+++ priceheker/scraper.py
@@ -11,7 +11,11 @@ from selenium import webdriver
 import time
 from selenium.webdriver import ChromeOptions, Chrome
 from selenium import webdriver
-
+from selenium import webdriver
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
 
 def xx(url):
  chrome_options = webdriver.ChromeOptions()
